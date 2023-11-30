0a12b2a81a065f99a5e014782842f91cac94b25f
kyaloeriq <kyaloeriq@gmail.com>
2023-11-30 12:54:08 -0800

    reading of the user input

--- simple_shell.c
+++ simple_shell.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - simple prompt
  * Return: 0
  */
 int main(void)
 {
-	char *ipt;
+	char *input = 0;
+	size_t length = 0;
+	ssize_t read;
 
 	while (1)
 	{
-		printf("start typing:");
+		printf("Purposedriven#:");
+		read = getline(&input, &length, stdin);/*Read input from the user*/
+		if (read == -1)
+		{
+			perror("getline");
+			exit(EXIT_FAILURE);
 	}
+		/*Execute commands from the input*/
+	}
+	free(input);
 	return (0);
 }
-		
