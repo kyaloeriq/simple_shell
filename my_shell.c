114830d85290b799f2be4c6402fe944168157858
kyaloeriq <kyaloeriq@gmail.com>
2023-12-04 06:54:32 -0800

    UNIX command line interpreter

--- my_shell.c
+++ my_shell.c
@@ -25,41 +25,44 @@ char *my_strdup(const char *s)
  */
 int main(void)
 {
-	char *command = "ls";
-	char *cmd = my_strdup(command);
-	char *tkn = strtok(cmd, " ");
-	char *argv[MAX_ARGS + 1];
-	int i;
-	int a = 0;
-	char *exectble = "/usr/bin/ls";
+	while (1)
+	{
+		char *command = "ls";
+		char *cmd = my_strdup(command);
+		char *tkn = strtok(cmd, " ");
+		char *argv[MAX_ARGS + 1];
+		int i;
+		int a = 0;
+		char *exectble = "/usr/bin/ls";
 
-	prompt();
-	/*Check if user specified a different command*/
-	if (tkn != NULL)
-	{
-		if (strcmp(tkn, "cp") == 0)
+		prompt();
+		/*Check if user specified a different command*/
+		if (tkn != NULL)
 		{
-			exectble = "/usr/bin/cp";
+			if (strcmp(tkn, "cp") == 0)
+			{
+				exectble = "/usr/bin/cp";
+			}
 		}
-	}
-	while (tkn != NULL && a < MAX_ARGS)
-	{
-		argv[a++] = my_strdup(tkn);
-		tkn = strtok(NULL, " ");
-	}
-	argv[a] = NULL;
-	/*Check if the executable file is accessible*/
-	if (access(exectble, X_OK) == -1)
-	{
-		fprintf(stderr, "Error:%s command not found\n", exectble);
-		exit(EXIT_FAILURE); 
-	}
-	forkExec(exectble, argv);
-	/*free dynamically allocated memory*/
-	free(cmd);
-	for (i = 0; i < a; ++i)
-	{
-		free(argv[i]);
+		while (tkn != NULL && a < MAX_ARGS)
+		{
+			argv[a++] = my_strdup(tkn);
+			tkn = strtok(NULL, " ");
+		}
+		argv[a] = NULL;
+		/*Check if the executable file is accessible*/
+		if (access(exectble, X_OK) == -1)
+		{
+			fprintf(stderr, "Error:%s command not found\n", exectble);
+			continue; 
+		}
+		forkExec(exectble, argv);
+		/*free dynamically allocated memory*/
+		free(cmd);
+		for (i = 0; i < a; ++i)
+		{
+			free(argv[i]);
+		}
 	}
 	return (0);
 }
