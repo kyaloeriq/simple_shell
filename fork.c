c26a38c17143976ef034793a27a29bf4a4a433b5
kyaloeriq <kyaloeriq@gmail.com>
2023-12-04 05:27:52 -0800

    fork and command execution

---
+++ fork.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "main.h"
+
+void forkExec(char *exectble, char *argv[])
+{
+	pid_t pid = fork();
+
+	if (pid == -1)
+	{
+		perror("Fork error");
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)
+	{
+		if(execve(exectble, argv, NULL) == -1)
+		{
+			perror("Command execution error");
+			exit(EXIT_FAILURE);
+		}
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
