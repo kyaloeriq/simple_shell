06aa52e997e20c78f6dd3b0108181a32db2ee851
kyaloeriq <kyaloeriq@gmail.com>
2023-12-04 05:43:54 -0800

    Command line including PATH

--- exec.c
+++ exec.c
@@ -29,8 +29,8 @@ void execCmd(char *command)
 	char *cmd = my_strdup(command);
 	char *tkn = strtok(cmd, " ");
 	char *argv[20];
+	int i;
 	int a = 0;
-	pid_t pid;
 	char *exectble = "/usr/bin/ls";
 
 	/*Check if user specified a different command*/
@@ -43,35 +43,21 @@ void execCmd(char *command)
 	}
 	while (tkn != NULL && a < 19)
 	{
-		argv[a++] = tkn;
+		argv[a++] = my_strdup(tkn);
 		tkn = strtok(NULL, " ");
 	}
 	argv[a] = NULL;
-
 	/*Check if the executable file is accessible*/
 	if (access(exectble, X_OK) == -1)
 	{
-		fprintf(stderr, "Error: %s command not found\n", exectble);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "Error:%s command not found\n", exectble);
+		exit(EXIT_FAILURE); 
 	}
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("Fork error");
-		exit(EXIT_FAILURE);
-	}
-	if (pid == 0)
-	{
-		if (execve(exectble, argv, NULL) == -1)
-		{
-			perror("Command execution error");
-			exit(EXIT_FAILURE);
-		}
-		exit(EXIT_SUCCESS);
-	}
-	else
-	{
-		wait(NULL);
-	}
+	forkExec(exectble, argv);
+	/*free dynamically allocated memory*/
 	free(cmd);
+	for (i = 0; i < a; ++i)
+	{
+		free(argv[i]);
+	}
 }
