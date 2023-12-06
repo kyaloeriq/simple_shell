b9ddc2fc7cdf57d5ad4260e9f51a3fdb34e947eb
kyaloeriq <kyaloeriq@gmail.com>
2023-12-06 13:04:23 -0800

    fork and command execution

--- fork.c
+++ fork.c
@@ -15,33 +15,32 @@ void forkExec(char *command, char *argv[])
 	char *path, *token, *exectblePath;
 	int status;
 
-	if (pid == -1)
+	path = getenv("PATH");
+	token = strtok(path, ":");
+	while (token != NULL)
 	{
-		perror("Fork error");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-		path = getenv("PATH");
-		token = strtok(path, ":");
-		while (token != NULL)
+		exectblePath = malloc(strlen(token) + strlen(command) + 2);
+		sprintf(exectblePath, "%s/%s", token, command);
+		if (access(exectblePath, X_OK) == 0)
 		{
-			exectblePath = malloc(strlen(token) + strlen(command) + 2);
-			sprintf(exectblePath, "%s/%s", token, command);
-			if (access(exectblePath, X_OK) == 0)
+			if (pid == -1)
 			{
+				perror("Fork error");
+				exit(EXIT_FAILURE);
+			}
+			else if (pid == 0)
+			{ /*Child process*/
 				argv[0] = strdup(exectblePath);
 				argv[1] = NULL;/*argv is terminated with NULL*/
 				execv(exectblePath, argv);
 				perror("Execution error");
 				exit(EXIT_FAILURE);
 			}
+		else/*Parent process*/
+			waitpid(pid. &status, 0);
 		free(exectblePath), token = strtok(NULL, ":");
 		}
 		/*if loop completes, command not found*/
 		fprintf(stderr, "Error: %s command not found\n", command);
 		exit(EXIT_FAILURE);
-	}
-	else
-		waitpid(pid, &status, 0);
 }
