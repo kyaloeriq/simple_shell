85221d5af1d9b1379518018f19998db00146e2dd
kyaloeriq <kyaloeriq@gmail.com>
2023-12-05 23:58:38 -0800

    my personal shell

--- my_shell.c
+++ my_shell.c
@@ -12,39 +12,35 @@ int main(void)
 {
 	char *command = NULL, *cmd = NULL, *tkn = NULL, *argv[MAX_ARGS + 1];
 	int i, a = 0;
-	char *exectble = "/usr/bin/ls";
+	char *exectblePath = NULL;
 
-	while (1)
-	{
+	while (1) {
 		prompt(&command);
-		if (strcmp(command, "exit") == 0)
-		{
+		if (strcmp(command, "exit") == 0) {
 			free(command);
 			break;
 		}
 		cmd = strdup(command);
 		tkn = strtok(cmd, " ");
 		/*Check if user specified a different command*/
-		if (tkn != NULL && strcmp(tkn, "cp") == 0)
-		{
-			exectble = "/usr/bin/cp";
+		if (tkn != NULL) {
+			exectblePath = tkn;
 		} a = 0;
-		while (tkn != NULL && a < MAX_ARGS)
-		{
+		while (tkn != NULL && a < MAX_ARGS) {
 			argv[a++] = strdup(tkn);
 			tkn = strtok(NULL, " ");
 		} argv[a] = NULL;
 		/*Check if the executable file is accessible*/
-		if (access(exectble, X_OK) == -1)
-		{
-			fprintf(stderr, "Error:%s command not found\n", exectble);
+		if (access(exectblePath, X_OK) == -1) {
+			fprintf(stderr, "Error:%s command not found\n", exectblePath);
 			free(command), free(cmd);
-			for (i = 0; i < a; ++i)
+			for (i = 0; i < a; ++i) {
 				free(argv[i]);
+				argv[i] = NULL;
+			}
 			continue;
 		}
-		argv[a] = NULL;
-		forkExec(exectble, argv);
+		forkExec(exectblePath, argv);
 		/*free dynamically allocated memory*/
 		free(command), free(cmd);
 		for (i = 0; i < a; ++i)
