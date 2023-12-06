7a3936f578346e5b4dc55ff3eaf4124d33e3f407
kyaloeriq <kyaloeriq@gmail.com>
2023-12-06 13:57:18 -0800

    Fork and command execution

--- fork.c
+++ fork.c
@@ -38,11 +38,9 @@ void forkExec(char *command, char *argv[])
 			}
 		free(exectblePath), token = strtok(NULL, ":");
 		/*if loop completes, command not found*/
+		}
 		fprintf(stderr, "Error: %s command not found\n", command);
 		else
-		{
-		
-		}	waitpid(pid, &status, 0);
-		}
+			waitpid(pid, &status, 0);
 	}
 }
