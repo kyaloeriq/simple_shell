4e97845e6f32873fb5e8d5402fb7f0707cd1cf55
kyaloeriq <kyaloeriq@gmail.com>
2023-12-06 22:49:51 -0800

    prints the current environment

--- env.c
+++ env.c
@@ -6,7 +6,9 @@
 void printEnv()
 {
 	extern char **environ;
-	for (char **env = environ; *env != NULL; env++)
+	char **env;
+
+	for (env = environ; *env != NULL; env++)
 	{
 		printf("%s\n", *env);
 	}
