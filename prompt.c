cb0774771b9dfc8e511346160dec304977f02d60
kyaloeriq <kyaloeriq@gmail.com>
2023-12-09 01:12:52 -0800

    my simple prompt

--- prompt.c
+++ prompt.c
@@ -12,8 +12,15 @@
 void prompt(char **command)
 {
 	size_t size = 0;
+	const char prompt_msg[] = "Purposedriven#: ";
+	const char empty_msg[] = "Command empty, consider entering a valid one\n";
 
-	printf("Purposedriven#: ");
+	if (write(STDOUT_FILENO, prompt_msg, sizeof(prompt_msg) - 1) == -1)
+	{
+		perror("Write error");
+		exit(EXIT_FAILURE);
+	}
+	/*Use getline to read user input*/
 	if (getline(command, &size, stdin) == -1)
 	{
 		perror("Input error");
@@ -23,7 +30,12 @@ void prompt(char **command)
 	(*command)[strcspn(*command, "\n")] = '\0';
 	if ((*command)[0] == '\0')
 	{
-		printf("Command empty, consider entering a valid one\n");
+		if (write(STDOUT_FILENO, empty_msg, sizeof(empty_msg) - 1) == -1)
+		{
+			perror("Write error");
+			exit(EXIT_FAILURE);
+		}
+		/*Recursive call for prompt to get a non_empty command*/
 		prompt(command);
 	}
 }
