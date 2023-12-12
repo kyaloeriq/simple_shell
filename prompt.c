412a970855c230968634cad5692072714e0a2396
kyaloeriq <kyaloeriq@gmail.com>
2023-12-12 07:09:21 -0800

    my simple prompt

--- prompt.c
+++ prompt.c
@@ -14,23 +14,27 @@ int prompt(char *command)
 	size_t size;
 	const char prompt_msg[] = "Purposedriven#: ";
 	const char empty_msg[] = "Command empty, consider entering a valid one\n";
-	
+	ssize_t bytesRead;
+
 	if (write(STDOUT_FILENO, prompt_msg, sizeof(prompt_msg) - 1) == -1)
 	{
 		perror("Write error");
 		exit(EXIT_FAILURE);
 	}
-	/*Use getline to read user input*/
-	if (my_getline(STDIN_FILENO, command, MAX_COMMAND_LENGTH) <= 0)
+	bytesRead = my_getline(STDIN_FILENO, command, MAX_COMMAND_LENGTH);
+	if (bytesRead == -1)
 	{
 		perror("Input error");
 		exit(EXIT_FAILURE);
 	}
-	/*Remove newline character at the end of *command*/
-	size = strlen(command);
-	if (size > 0 && command[size - 1] == '\n')
+	else
 	{
-		command[size - 1] = '\0';
+	/*Remove newline character at the end of *command*/
+		size = strlen(command);
+		if (size > 0 && command[size - 1] == '\n')
+		{
+			command[size - 1] = '\0';
+		}
 	}
 	if (command[0] == '\0')
 	{
