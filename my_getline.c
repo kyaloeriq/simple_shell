9581e4df16971dbd8a96fafb34a59a535f8555e4
kyaloeriq <kyaloeriq@gmail.com>
2023-12-12 07:41:54 -0800

    my own getline

--- my_getline.c
+++ my_getline.c
@@ -13,48 +13,17 @@
  */
 ssize_t my_getline(int fd, char *line, size_t maxchar)
 {
-	ssize_t bytesRead = 0;
-	ssize_t tlBytsRead = 0;
+	ssize_t bytesRead = read(fd, line, maxchar);
 
-	if (maxchar == 0 || line == NULL)
+	if (bytesRead == -1)
 	{
+		perror("Error reading from file");
 		return (-1);
 	}
-	while (tlBytsRead < (ssize_t)maxchar - 1)
+	if (bytesRead > 0 && line[bytesRead - 1] == '\n')
 	{
-		bytesRead = read(fd, line + tlBytsRead, 1);
-		if (bytesRead > 0)
-		{
-			tlBytsRead += bytesRead;
-			if (line[tlBytsRead - 1] == '\n')
-			{
-				line[tlBytsRead] = '\0';/*Remove newline character*/
-				return (tlBytsRead);
-			}
-		}
-		else if (bytesRead == 0 && tlBytsRead == 0)
-		{
-			return (0); /*Reached EOF without any bytes*/
-		}
-		else if (bytesRead == 0)
-		{
-			break;/*Reached EOF after reading some bytes*/
-		}
-		else if (bytesRead == -1 && errno != EINTR)
-		{
-			perror("Error reading from file");
-			return (-1);/*Error reading from file*/
-		}
-		else if (bytesRead == -1 && errno == EINTR)
-		{
-			perror("Error reading from file");
-			return (-1);
-		}
+		line[bytesRead - 1] = '\0';/*Remove newline character*/
+		return (bytesRead);
 	}
-	if (tlBytsRead > 0)
-	{
-		line[tlBytsRead] = '\0';
-		return (tlBytsRead);
-	}
-	return (-1);
+	return (bytesRead);
 }
