c767088a2e758ba8ee0db01186b21041d2c6f6b3
kyaloeriq <kyaloeriq@gmail.com>
2023-12-12 02:32:58 -0800

    my personal getline

--- my_getline.c
+++ my_getline.c
@@ -3,15 +3,15 @@
 #include "main.h"
 /**
  * my_getline - my personal getline
- * @f: File pointer
+ * @fd: File descriptor
  * @line: Buffer for storing the line
  * @maxchar: Maximum number of characters to read
+ * Return: bytes read, -1 incase of error
  */
 ssize_t my_getline(int fd, char *line, size_t maxchar)
 {
 	ssize_t bytesRead = 0;
 	ssize_t tlBytsRead = 0;
-	
 
 	if (maxchar == 0 || line == NULL)
 	{
