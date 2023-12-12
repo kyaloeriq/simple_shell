8c4961213910fc3f2c942c974c7e31b961664cf2
kyaloeriq <kyaloeriq@gmail.com>
2023-12-12 01:16:25 -0800

    execution of commands

--- exec.c
+++ exec.c
@@ -15,7 +15,7 @@ int execCmd(char *command, char *argv[], int exitStat)
 {
 	char *path, *token, *exectblePath;
 	int a;
-	
+
 	if (strchr(command, '/') != NULL)
 	{ /*Likely an absolute path*/
 		execv(command, argv);
@@ -26,24 +26,20 @@ int execCmd(char *command, char *argv[], int exitStat)
 	{
 		if (argv[1] != NULL)
 		{
-			exitStat = atoi(argv[1]);
-		}
+			exitStat = atoi(argv[1]); }
 		exit(exitStat);/*Exit shell with specified status*/
 	}
 	for (a = 0; environ[a] != NULL; ++a)
 	{
 		if (strncmp(environ[a], "PATH=", 5) == 0)
-		{
-			path = environ[a] + 5;
-			break;
-		}
+		{	path = environ[a] + 5;
+			break; }
 	} /*if PATH is not in environ, print an error*/
 	if (path == NULL)
 	{
 		perror("Error: PATH variable not found\n");
 		exit(EXIT_FAILURE);
-	}
-	token = strtok(path, ":");
+	} token = strtok(path, ":");
 	while (token != NULL)
 	{
 		exectblePath = malloc(strlen(token) + strlen(command) + 2);
@@ -54,9 +50,7 @@ int execCmd(char *command, char *argv[], int exitStat)
 			perror("Execution error");
 			free(exectblePath);
 		}
-		free(exectblePath);
-		token = strtok(NULL, ":");
+		free(exectblePath), token = strtok(NULL, ":");
 	} /*if loop completes, command not found*/
-	perror("Command not found");
-	exit(exitStat);
+	perror("Command not found"), exit(exitStat);
 }
