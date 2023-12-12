ed3611a713a5d4d70eae6856f54d346bc9bd4454
kyaloeriq <kyaloeriq@gmail.com>
2023-12-12 02:29:02 -0800

    my personal getline

---
+++ my_getline.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * my_getline - my personal getline
+ * @f: File pointer
+ * @line: Buffer for storing the line
+ * @maxchar: Maximum number of characters to read
+ */
+ssize_t my_getline(int fd, char *line, size_t maxchar)
+{
+	ssize_t bytesRead = 0;
+	ssize_t tlBytsRead = 0;
+	
+
+	if (maxchar == 0 || line == NULL)
+	{
+		return (-1);
+	}
+	while (tlBytsRead < maxchar - 1)
+	{
+		bytesRead = read(fd, line + tlBytsRead, 1);
+		if (bytesRead > 0)
+		{
+			if (s[tlBytsRead] == '\n')
+			{
+				s[tlBytsRead] = '\0';/*Remove newline character*/
+				return (tlBytsRead);
+			}
+			tlBytsRead += bytesRead;
+		}
+		else if (bytesRead == 0)
+		{
+			break; /*Reached EOF*/
+		}
+		else
+		{
+			return (-1);/*Error reading from file*/
+		}
+	}
+	return (tlBytsRead);
+}
