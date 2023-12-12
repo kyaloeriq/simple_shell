fc37673ca6901286d579407dd872ad62e267bdcf
kyaloeriq <kyaloeriq@gmail.com>
2023-12-12 02:40:17 -0800

    header file

--- main.h
+++ main.h
@@ -6,6 +6,7 @@ char *my_strdup(const char *s);
 int execCmd(char *command, char *argv[], int exitStat);
 void forkExec(char *command, char *argv[]);
 void printEnv();
+ssize_t my_getline(int fd, char *line, size_t maxchar);
 
 #define MAX_ARGS 20
 extern char **environ;
