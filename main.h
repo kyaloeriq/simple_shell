ad1f2fdc7b9e7e04acef6fe4703b2bbc904fec68
kyaloeriq <kyaloeriq@gmail.com>
2023-12-12 05:14:22 -0800

    header file

--- main.h
+++ main.h
@@ -9,7 +9,7 @@ void printEnv();
 ssize_t my_getline(int fd, char *line, size_t maxchar);
 
 #define MAX_ARGS 20
-#define MAX_COMMAND_LENGTH 256
+#define MAX_COMMAND_LENGTH 1024
 extern char **environ;
 
 #endif
