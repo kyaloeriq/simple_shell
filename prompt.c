3ad3ec83897de0ca3ac3b922b067436760795f15
kyaloeriq <kyaloeriq@gmail.com>
2023-12-13 02:20:35 -0800

    my simple prompt

--- prompt.c
+++ prompt.c
@@ -16,35 +16,40 @@ int prompt(char *command)
 	const char empty_msg[] = "Command empty, consider entering a valid one\n";
 	ssize_t bytesRead;
 
-	if (write(STDOUT_FILENO, prompt_msg, sizeof(prompt_msg) - 1) == -1)
+	while (1)
 	{
-		perror("Write error");
-		exit(EXIT_FAILURE);
-	}
-	bytesRead = my_getline(STDIN_FILENO, command, MAX_COMMAND_LENGTH);
-	if (bytesRead == -1)
-	{
-		perror("Input error");
-		return (-1);
-	}
-	else if (bytesRead == 0)
-	{
-		return (0);/*Handle EOF*/
-	}
-	/*Remove newline character at the end of command*/
-	size = strlen(command);
-	if (size > 0 && command[size - 1] == '\n')
-	{
-		command[size - 1] = '\0';
-	}
-	if (command[0] == '\0')
-	{
-		if (write(STDERR_FILENO, empty_msg, sizeof(empty_msg) - 1) == -1)
+		if (write(STDOUT_FILENO, prompt_msg, sizeof(prompt_msg) - 1) == -1)
 		{
-			perror("Input error");
+			perror("Write error");
 			exit(EXIT_FAILURE);
 		}
-		return (0);/*Command is empty*/
+		bytesRead = my_getline(STDIN_FILENO, command, MAX_COMMAND_LENGTH);
+		if (bytesRead == -1)
+		{
+			perror("Input error");
+			return (-1);
+		}
+		else if (bytesRead == 0)
+		{
+			return (0);/*Handle EOF*/
+		}
+		/*Remove newline character at the end of command*/
+		size = strlen(command);
+		if (size > 0 && command[size - 1] == '\n')
+		{
+			command[size - 1] = '\0';
+		}
+		if (command[0] == '\0')
+		{
+			if (write(STDERR_FILENO, empty_msg, sizeof(empty_msg) - 1) == -1)
+			{
+				perror("Input error");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			return (1);/*Indicates a valid non-empty command*/
+		}
 	}
-	return (1);/*Indicates a valid non-empty command*/
 }
