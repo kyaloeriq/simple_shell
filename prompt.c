aeef2d3370d560a105b9a714b15cb89f6b4e777a
kyaloeriq <kyaloeriq@gmail.com>
2023-12-13 00:27:06 -0800

    my simple prompt

--- prompt.c
+++ prompt.c
@@ -39,7 +39,7 @@ int prompt(char *command)
 			command[size - 1] = '\0'; }
 		if (command[0] == '\0')
 		{
-			if (write(STDOUT_FILENO, empty_msg, sizeof(empty_msg)) == -1)
+			if (write(STDOUT_FILENO, empty_msg, sizeof(empty_msg) - 1) == -1)
 			{
 				perror("Write error");
 				exit(EXIT_FAILURE); }
@@ -48,7 +48,7 @@ int prompt(char *command)
 		{
 			if (!valCmds(command))
 			{
-				if (write(STDERR_FILENO, not_found_msg, strlen(not_found_msg) - 1) == -1)
+				if (write(STDERR_FILENO, not_found_msg, strlen(not_found_msg)) == -1)
 				{
 					perror("Write error");
 					exit(EXIT_FAILURE); }
