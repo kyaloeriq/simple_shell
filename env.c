2fd43f552345330dcc896a850e52c2a49c495fa0
kyaloeriq <kyaloeriq@gmail.com>
2023-12-13 14:43:56 -0800

    prints the current environment

--- env.c
+++ env.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 /**
  * printEnv - prints the current environment
@@ -6,9 +7,14 @@
 void printEnv(void)
 {
 	char **env;
+	size_t len = 0;
 
 	for (env = environ; *env != NULL; env++)
 	{
-		printf("%s\n", *env);
+		while ((*env)[len] != '\0')
+		len++;
+
+		write(STDOUT_FILENO, *env, len);
+		write(STDOUT_FILENO, "\n", 1);
 	}
 }
